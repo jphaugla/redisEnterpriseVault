766c9d3a7b79e92531df5debfe7f25d413099b82
Jason Haugland <jasonhaugland@gmail.com>
2022-12-28 17:20:10 -0600

    no autoscaling

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -8,14 +8,9 @@ variable "gke_password" {
   description = "gke password"
 }
 
-variable "gke_min_nodes" {
-  default     = 3
-  description = "min number of gke nodes "
-}
-
-variable "gke_max_nodes" {
-  default     = 6
-  description = "max number of gke nodes "
+variable "gke_num_nodes" {
+  default     = 1
+  description = "number of gke nodes per zone"
 }
 
 # GKE cluster
@@ -38,10 +33,7 @@ resource "google_container_node_pool" "primary_nodes" {
   name       = "${google_container_cluster.primary.name}"
   location   = var.region
   cluster    = google_container_cluster.primary.name
-  autoscaling {
-    min_node_count = var.gke_min_nodes
-    max_node_count = var.gke_max_nodes
-  }
+  node_count = var.gke_num_nodes
 
   node_config {
     oauth_scopes = [
