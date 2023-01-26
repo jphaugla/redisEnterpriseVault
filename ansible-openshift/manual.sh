3c9f5e0c60b48d683a35b87b623e42c9c46117e3
Jason Haugland <jasonhaugland@gmail.com>
2023-01-26 08:03:20 -0600

    add openshift ansible

---
+++ ansible-openshift/manual.sh
@@ -0,0 +1,1 @@
+ansible-playbook  -vvvv --extra-vars cluster_state=present playbook.yml -e 'cluster_name=jph-openshift'  -e 'gcp_region=us-central1' -e 'gcp_project=central-beach-194106' -e 'ansible_python_interpreter=/usr/local/bin/python3' -e 'openshift_installer_url=https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp' -e 'openshift_version=4.11.3'
