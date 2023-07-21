3b7dff8543cf6f8a30e57d240ad17cca2b1cbe65
Jason Haugland <jasonhaugland@gmail.com>
2023-07-21 13:58:43 -0500

    fix postgres connection error for rdi

--- rdi/redis-di-create.sh
+++ rdi/redis-di-create.sh
@@ -1,1 +1,1 @@
-redis-di create --cluster-host test-rec.demo.svc.cluster.local --cluster-api-port 9443 --cluster-user demo@redis.com --cluster-password aN56lDfy --rdi-port 12002 --rdi-password jasonrocks --silent
+redis-di create --cluster-host test-rec.demo.svc.cluster.local --cluster-api-port 9443 --cluster-user demo@redis.com --cluster-password nvcJG2RS --rdi-port 12002 --rdi-password jasonrocks --silent
